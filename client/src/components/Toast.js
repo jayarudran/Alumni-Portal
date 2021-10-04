3dbd958cfe876806d4a73b3fc7f6e6b0739d7fc3
RheaAdh <rheadhikari@gmail.com>
2021-10-04 16:40:42 +0530

    merged

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -1,14 +1,15 @@
+import React from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 const Toast = ({ message }) => {
-  const notify = () => toast(message);
-  return (
-    <div>
-      {/* <button onClick={notify}>{message}</button> */}
-      <h4 style={{ display: "none" }}>{notify()}</h4>
-      <Toaster />
-    </div>
-  );
+    const notify = () => toast(message);
+    return (
+        <div>
+            {/* <button onClick={notify}>{message}</button> */}
+            <h4 style={{ display: "none" }}>{notify()}</h4>
+            <Toaster />
+        </div>
+    );
 };
 
 export default Toast;
