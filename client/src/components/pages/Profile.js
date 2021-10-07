ee6d99d8c36f015bdf1ede54390c22b987dd5d16
Avanish Singh <76589058+Averek7@users.noreply.github.com>
2021-10-07 16:48:05 +0530

    Update Profile.js

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -14,7 +14,7 @@ import MyPhoto from '../../images/me.jpg';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 
-const Profile = (props) => {
+const Profile = () => {
     const auth = useAuth();
     const profileFileRef = useRef(null);
     const history = useHistory();
@@ -46,7 +46,6 @@ const Profile = (props) => {
 
     const handleUploadImage = async (e) => {
         e.preventDefault();
-        props.Progress(0);
         const profileImage = await handleSaveDocument(
             'profile',
             Date.now().toString(),
@@ -74,12 +73,10 @@ const Profile = (props) => {
                 console.log(auth.user);
             })
             .catch((err) => console.log(err));
-        props.Progress(100);
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.Progress(0);
         console.log('submit edit profile');
         axios({
             method: 'put',
@@ -110,7 +107,6 @@ const Profile = (props) => {
                 console.log(auth.user);
             })
             .catch((err) => console.log(err));
-        props.Progress(100);
         window.location.reload();
     };
 
