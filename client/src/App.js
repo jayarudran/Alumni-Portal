2dd0128b299f4bb5031554538ce63ef6b0651bbd
Avanish Singh <76589058+Averek7@users.noreply.github.com>
2021-10-07 16:43:50 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -38,60 +38,60 @@ const App = () => {
                 {/* Private Routes */}
                 <PrivateRoute Progress={Progress} exact path={"/"} component={Feed}></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+         
                     path="/community"
                     component={Community}
                 ></PrivateRoute>
                 <PrivateRoute Progress={Progress} path="/chat" component={Messages}></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     exact
                     path="/profile"
                     component={Profile}
                 ></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     path="/saved"
                     component={Bookmarks}
                 ></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     exact
                     path="/profile/:username"
                     component={OtherProfile}
                 ></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     exact
                     path="/post/:postid"
                     component={RandomPost}
                 ></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     path="/admin"
                     component={AdminPage}
                 ></PrivateRoute>
                 <PrivateRoute
-                    Progress={Progress}
+                    
                     path="/post/:postid"
                     component={Post}
                 ></PrivateRoute>
                 {/* Public routes */}
                 <Route
-                    Progress={Progress}
+                    
                     path="/forgotpassword"
                     component={ForgotPassword}
                 ></Route>
                 <Route
-                    Progress={Progress}
+                    
                     path="/resetpassword/:token"
                     component={ResetPassword}
                 ></Route>
                 <Route Progress={Progress} path="/register" component={Register}></Route>
                 <Route Progress={Progress} path="/login" component={Login}></Route>
-                <Route Progress={Progress} path="/verifyemail/:token" component={Verified}></Route>
+                <Route  path="/verifyemail/:token" component={Verified}></Route>
                 <Route Progress={Progress} path="/verifyagain" component={VerifyAgain}></Route>
-                <Route Progress={Progress} path="*" component={NotFound} />
+                <Route  path="*" component={NotFound} />
             </Switch>
         </React.Fragment>
     );
